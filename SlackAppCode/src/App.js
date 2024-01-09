90339f53b28aa6a988464f5b6c2b35c3696507a9
Sonali <sonalipatil511999@gmail.com>
2024-01-09 13:15:35 +0530

    feature/slack-01-initital-changes

--- SlackAppCode/src/App.js
+++ SlackAppCode/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Slack App Starts from here
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Used React
         </a>
       </header>
     </div>
